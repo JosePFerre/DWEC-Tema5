44ae4c5d506884a8c314421d9e4e4f8a53a69508
Jose Pablo Ferre <Jose Pablo Ferre>
2016-12-04 18:50:14 +0100

    99.99% terminado

--- Snake/juego.js
+++ Snake/juego.js
@@ -1,3 +1,6 @@
+//RELACION 2 TEMA 5 DAW DWEC
+//Jose Pablo Ferre Leorini
+
 class Juego{
     constructor(){
         this.svg = document.getElementsByTagName('svg')[0];
@@ -105,22 +108,24 @@ function reload(){
     location.reload();
 }
 
+
 window.onkeydown = function(event) {
-   if ((event.keyCode == 38 || event.keyCode == 87) && dir_actual != 'abajo') {
-        clearInterval(idInt);
-        idInt = setInterval(esto.mover,75,'arriba');
-        dir_actual='arriba';
-   } else if((event.keyCode == 40 || event.keyCode == 83) && dir_actual != 'arriba'){
-        clearInterval(idInt);
-        idInt = setInterval(esto.mover,75,'abajo');
-        dir_actual='abajo';
-   } else if((event.keyCode == 37 || event.keyCode == 65) && dir_actual != 'der'){
-        clearInterval(idInt);
-        idInt = setInterval(esto.mover,75,'izq');
-        dir_actual='izq';
-   } else if((event.keyCode == 39 || event.keyCode == 68) && dir_actual != 'izq'){
-        clearInterval(idInt);
-        idInt = setInterval(esto.mover,75,'der');
-        dir_actual='der';
-   }
+        if ((event.keyCode == 38 || event.keyCode == 87) && dir_actual != 'abajo') {
+            clearInterval(idInt);
+            idInt = setInterval(esto.mover,75,'arriba');
+            dir_actual='arriba';
+        } else if((event.keyCode == 40 || event.keyCode == 83) && dir_actual != 'arriba'){
+            clearInterval(idInt);
+            idInt = setInterval(esto.mover,75,'abajo');
+            dir_actual='abajo';
+        } else if((event.keyCode == 37 || event.keyCode == 65) && dir_actual != 'der'){
+            clearInterval(idInt);
+            idInt = setInterval(esto.mover,75,'izq');
+            dir_actual='izq';
+        } else if((event.keyCode == 39 || event.keyCode == 68) && dir_actual != 'izq'){
+            clearInterval(idInt);
+            idInt = setInterval(esto.mover,75,'der');
+            dir_actual='der';
+        }
+
 }
